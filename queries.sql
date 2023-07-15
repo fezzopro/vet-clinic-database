63fb4a01abe9e5b532b787eca4a46f3c32900782
Fezzopro <fezzopro@gmail.com>
2023-07-15 09:54:31 +0200

    Add SAVEPOINT statement to the transaction

--- queries.sql
+++ queries.sql
@@ -53,3 +53,15 @@ BEGIN TRANSACTION;
 	DELETE FROM animals;
 ROLLBACK TRANSACTION;
 SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+	DELETE FROM animals
+	WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+	UPDATE animals
+	SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT my_savepoint;
+	UPDATE animals SET weight_kg = weight_kg * -1
+	WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
